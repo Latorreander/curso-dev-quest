43edec1371f0b98fc214ae725f572fc6ab74b221
Latorreander <latorreander@gmail.com>
2022-05-05 12:16:33 -0300

    estudos de condicionais

---
+++ modulo-javascript-basico/09-condicionais/index.js
@@ -0,0 +1,24 @@
+//if(se), else(se não), else if(se for)
+
+// let nomeFilme = 'Vingadores'
+
+// if(nomeFilme === 'Vingadores'){
+//     console.log('É o filme dos Vingadores')
+// }else{
+//     console.log('É outro filme');
+// }
+
+//o else if, usamos para algo específico
+
+let nomeSerie = 'Prision'
+
+if(nomeSerie === 'Breaking Bad'){
+    console.log('É a série do professor cozinheiro');
+}else if(nomeSerie === 'Prision Break'){
+    console.log('É a série do cara que tenta fugir da prisão');
+}else{
+    console.log('Éssa série eu não conheço');
+}
+
+
+
