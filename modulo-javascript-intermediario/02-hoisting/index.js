6887adadc68d725b889e28e9adf6ccafd35ea059
Latorreander <latorreander@gmail.com>
2022-05-25 11:21:41 -0300

    funções anonimas

---
+++ modulo-javascript-intermediario/02-hoisting/index.js
@@ -0,0 +1,7 @@
+var pais //declaração
+pais = 'Brasil' //atribuição
+
+var pais = 'Brasil' //na mesma linha a atribuição e a declaração, o js entende que são duas coisas separadas apesar de estarem juntas na mesma linha
+
+//todas as declarações de variáveis são movidas para o topo de ume escopo, isso é chamdo de hoisting.
+
---
+++ modulo-javascript-intermediario/03-funcoes-anonimas/index.js
@@ -0,0 +1,19 @@
+// function somar(numero1, numero2){
+//     return numero1 + numero2
+// }
+
+// let resultadoDaSoma = somar(12, 3)
+// console.log(resultadoDaSoma);
+
+
+
+// USANDO UMA FUNÇÃO ANÔNIMA
+
+let resultadoDaSoma = function(numero1, numero2){
+    return numero1 + numero2
+}
+
+
+console.log(resultadoDaSoma(12,6));
+
+// a função não recebe nome, ela está sendo armazenada dentro da variável, agora a variável é uma função, portanto chamamos a função com o nome da vairável e damos o valor.
