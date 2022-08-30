2b0c107288ae1deef2238ff0597eeca4b40409e9
Latorreander <latorreander@gmail.com>
2022-08-30 19:53:01 -0300

    js avançqado

--- modulo-javascript-avancado/15-requisicoes-com-fetch/index.js
+++ modulo-javascript-avancado/15-requisicoes-com-fetch/index.js
@@ -10,8 +10,10 @@ criarBaralhoEmbaralhado = async () => {
   return await resposta.json();
 };
 
+
+
 tirarUmaCarta = async (deck_id) => {
-  const url = `http://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`;
+  const url = `http://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`;
   const resposta = await fetch(url);
   return await resposta.json();
 };
@@ -19,8 +21,14 @@ tirarUmaCarta = async (deck_id) => {
 tirarUmaCartaAleatoriaDoBaralho = async () => {
   const baralho = await criarBaralhoEmbaralhado();
   const carta = await tirarUmaCarta(baralho.deck_id);
-  const imagemCarta = carta.cards[0].image;
-  document.getElementById("carta").src = imagemCarta;
+  const imagemCarta1 = carta.cards[0].image;
+  document.getElementById("carta1").src = imagemCarta1
+
+  const imagemcarta2 = carta.cards[1].image
+  document.getElementById('carta2').src =
+  imagemcarta2
+  console.log(carta);
+
 };
 
 tirarUmaCartaAleatoriaDoBaralho();
---
+++ modulo-javascript-avancado/16-gerenciando-pacotes-npm/index.js
@@ -0,0 +1,4 @@
+const kindOf =require('kind-of')
+const testeBoleano = true
+const testeDeTexto ='texto'
+console.log(kindOf(testeBoleano));
