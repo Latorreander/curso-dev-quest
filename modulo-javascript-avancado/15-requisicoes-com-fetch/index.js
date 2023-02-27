4dca1a32aac8fa32bd39d712cc31beb88f4890de
Latorreander <latorreander@gmail.com>
2023-02-27 11:42:42 -0300

    'alteração nome variável'

--- modulo-javascript-avancado/15-requisicoes-com-fetch/index.js
+++ modulo-javascript-avancado/15-requisicoes-com-fetch/index.js
@@ -1,34 +1,5 @@
-document.getElementById("tirar-carta").addEventListener("click", () => {
-    tirarUmaCartaAleatoriaDoBaralho();
-});
-
-async function criarBaralhoEmbaralhado() {
-    const url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
-    const resposta = await fetch(url);
-    return await resposta.json();
-}
-
-async function tirarUmaCarta(deck_id) {
-    const url = `https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`;
-    const resposta = await fetch(url);
-    return await resposta.json();
-}
-
-async function tirarUmaCartaAleatoriaDoBaralho() {
-    const baralho = await criarBaralhoEmbaralhado();
-    const carta = await tirarUmaCarta(baralho.deck_id);
-    const imagemCarta = carta.cards[0].image;
-
-    document.getElementById("carta1").src = imagemCarta;
-}
-tirarUmaCartaAleatoriaDoBaralho();
-
-//--------------------------------------------------------------------------
-
-
-
 // document.getElementById("tirar-carta").addEventListener("click", () => {
-//     tirarUmaCarta();
+//     tirarUmaCartaAleatoriaDoBaralho();
 // });
 
 // async function criarBaralhoEmbaralhado() {
@@ -37,16 +8,44 @@ tirarUmaCartaAleatoriaDoBaralho();
 //     return await resposta.json();
 // }
 
-// async function tirarUmaCarta() {
-
-//     const baralho = await criarBaralhoEmbaralhado();
-//     const url = `https://deckofcardsapi.com/api/deck/${baralho.deck_id}/draw/?count=1`;
+// async function tirarUmaCarta(deck_id) {
+//     const url = `https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`;
 //     const resposta = await fetch(url);
-//     const carta = await resposta.json();
-//     const imagemCarta = carta.cards[0].image;
-//     document.getElementById("carta1").src = imagemCarta;
+//     return await resposta.json();
 // }
 
-// tirarUmaCarta();
+// async function tirarUmaCartaAleatoriaDoBaralho() {
+//     const baralho = await criarBaralhoEmbaralhado();
+//     const carta = await tirarUmaCarta(baralho.deck_id);
+//     const imagemCarta = carta.cards[0].image;
+
+//     document.getElementById("carta").src = imagemCarta;
+// }
+// tirarUmaCartaAleatoriaDoBaralho();
+
+//--------------------------------------------------------------------------
+
+
+
+document.getElementById("tirar-carta").addEventListener("click", () => {
+    tirarUmaCartaAleatoria();
+});
+
+async function criarBaralhoEmbaralhado() {
+    const url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+    const resposta = await fetch(url);
+    return await resposta.json();
+}
+
+async function tirarUmaCartaAleatoria() {
+    const baralho = await criarBaralhoEmbaralhado();
+    const url = `https://deckofcardsapi.com/api/deck/${baralho.deck_id}/draw/?count=1`;
+    const resposta = await fetch(url);
+    const carta = await resposta.json();
+    const imagemCarta = carta.cards[0].image;
+    document.getElementById("carta").src = imagemCarta;
+}
+
+tirarUmaCartaAleatoria();
 
 
